12241a86b749779c6c0791c23a3ee803740d8cdd
duytan12a7 <dinomsju@gmail.com>
2024-12-09 01:43:13 +0700

    Init Project

---
+++ Assets/01 PREFABS/UI/JoyStick/JoyStick.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class JoyStick : MonoBehaviour, IDragHandler, IPointerDownHandler, IPointerUpHandler
+{
+    [SerializeField] private RectTransform centerTrans;
+    [SerializeField] private RectTransform backgroundTrans;
+    [SerializeField] private RectTransform thumbStickTrans;
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Vector2 touchPos = eventData.position;
+        Vector2 centerPos = backgroundTrans.position;
+
+        Vector2 localOffset = Vector2.ClampMagnitude(touchPos - centerPos, backgroundTrans.sizeDelta.x / 7);
+
+        thumbStickTrans.position = localOffset + centerPos;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        backgroundTrans.position = eventData.position;
+        thumbStickTrans.position = eventData.position;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        backgroundTrans.position = centerTrans.position;
+        thumbStickTrans.position = backgroundTrans.position;
+    }
+}
